a79b1810c2182d6cb793b3b8c957ecfd6e34dcc5
lorenzobruni-dev <109805268+lorenzobruni-dev@users.noreply.github.com>
2023-12-13 11:40:03 +0100

    Last changes

--- lollo-port/src/components/homepage/contentHomepage/ContentHomepage.tsx
+++ lollo-port/src/components/homepage/contentHomepage/ContentHomepage.tsx
@@ -1,8 +1,10 @@
 import BodyHomepage from "../bodyHomepage/BodyHomepage";
-import { Flex, Text, useMantineTheme } from "@mantine/core";
+import { Button, Flex, Text, useMantineTheme } from "@mantine/core";
+import { useLocation } from "wouter";
 
 const ContentHomepage = () => {
   const theme = useMantineTheme();
+  const [, setLocation] = useLocation();
   return (
     <BodyHomepage>
       <Flex direction={"column"} gap={10}>
@@ -16,6 +18,18 @@ const ContentHomepage = () => {
           Frontend Developer / React <br />
           Developer
         </Text>
+        <Button
+          variant="default"
+          onClick={() => setLocation("/contact")}
+          sx={{
+            backgroundColor: "inherit",
+            border: `1px solid ${theme.colors.yellow[3]}`,
+            color: theme.colors.yellow[3],
+            "&:hover": { backgroundColor: "inherit" },
+          }}
+        >
+          Contact me
+        </Button>
       </Flex>
     </BodyHomepage>
   );
--- lollo-port/src/pages/Homepage.tsx
+++ lollo-port/src/pages/Homepage.tsx
@@ -2,9 +2,12 @@ import { Flex } from "@mantine/core";
 import ContentHomepage from "../components/homepage/contentHomepage/ContentHomepage";
 
 const Homepage = () => {
+  const width = window.screen.width;
+  const isStrected = width <= 660;
+
   return (
     <Flex
-      pl={150}
+      pl={isStrected ? 50 : 150}
       direction={"column"}
       align={"center"}
       h={"100%"}
